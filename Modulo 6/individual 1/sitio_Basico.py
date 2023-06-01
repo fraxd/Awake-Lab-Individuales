150c4c72b599f3ad119d75fadff652079f6173cc
fraxd <fraxd98@gmail.com>
2023-06-01 18:55:25 -0400

    Update

---
+++ Modulo 6/individual 1/sitio_Basico.py
@@ -0,0 +1,17 @@
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+
+class MyHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"Servidor levantado mediante http.server\n")
+        self.wfile.write(b"hecho por Franco Fuentes S.")
+
+def run(server_class=HTTPServer, handler_class=MyHandler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f"Servidor en funcionamiento en el puerto {port}")
+    httpd.serve_forever()
+
+run()
