6d4a406b2bc5e68938a4f78e4add16a5a5816a94
fraxd <fraxd98@gmail.com>
2023-06-10 17:44:51 -0400

    individual 4 Listo

--- Modulo 6/Individual 4/awakeSystems/bancoSimi/urls.py
+++ Modulo 6/Individual 4/awakeSystems/bancoSimi/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('users/', views.user_list, name='user_list'),
+    path('usersBasico/', views.user_list_basico, name='user_list'),
     path('', views.landing_page, name='Landing Page'),
     path('crear-usuario/', views.crear_usuario, name='crear_usuario'),
 
--- Modulo 6/Individual 4/awakeSystems/bancoSimi/views.py
+++ Modulo 6/Individual 4/awakeSystems/bancoSimi/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from .models import User as usuarioBasico
 from .forms import UsuarioForm
 
 
@@ -13,13 +14,15 @@ def user_list(request):
     return render(request, 'usuarios.html', {'users': users})
 
 def user_list_basico(request):
-    users = User.objects.all()
-    return render(request, 'userBasicos.html', {'users': users})
+    usuariosBasicos = usuarioBasico.objects.all()
+    return render(request, 'userBasicos.html', {'users': usuariosBasicos})
 
-def crear_usuario(request):
+def crear_usuario(request): 
     if request.method == 'POST':
         form = UsuarioForm(request.POST)
+        print(form.errors)
         if form.is_valid():
+            print('Es valido')
             # Guardar los datos del formulario en la base de datos u otra lógica
             form.save()
             return render(request, 'exito.html')
