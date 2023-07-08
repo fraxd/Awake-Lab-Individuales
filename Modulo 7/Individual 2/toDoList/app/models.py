a87d7c16d9d6d9e6fc589871b3c7b4980812cee4
fraxd <fraxd98@gmail.com>
2023-07-08 13:08:08 -0400

    Actualizacion Nueva

---
+++ Modulo 7/Individual 2/toDoList/app/models.py
@@ -0,0 +1,51 @@
+import datetime
+from django.db import models
+from django.contrib.auth.models import User
+import uuid
+
+class Tarea(models.Model):
+    OPCIONES_ESTADO = [ # Talvez hacer una clase aparte ?
+        ('pendiente', 'Pendiente'),
+        ('en_progreso', 'En progreso'),
+        ('completada', 'Completada'),
+    ]
+    OPCIONES_ETIQUETA = [ # Talvez Hacer una clase
+        ('hogar', 'Hogar'),
+        ('universidad', 'Universidad'),
+        ('trabajo', 'Trabajo'),
+        ('salud', 'Salud'),
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    titulo = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    fecha_vencimiento = models.DateField()
+    estado = models.CharField(max_length=20, choices=OPCIONES_ESTADO, default='pendiente')
+    etiqueta = models.CharField(max_length=20, choices=OPCIONES_ETIQUETA)
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+    
+    def mod_estado(self, estado):
+        self.estado = estado
+        self.save()
+
+    def update_info(self,tituo, descripcion, fecha_vencimiento, etiqueta):
+        self.titulo = tituo
+        self.descripcion = descripcion
+        self.fecha_vencimiento = fecha_vencimiento
+        self.etiqueta = etiqueta
+        self.save()
+
+    @classmethod
+    def tareas_vencidas(cls):
+        return cls.objects.filter(fecha_vencimiento__lt=datetime.date.today())
+    
+    @classmethod
+    def tareas_por_etiqueta(cls, etiqueta):
+        return cls.objects.filter(etiqueta=etiqueta)
+    
+    @classmethod
+    def tareas_por_usuario(cls, usuario):
+        return cls.objects.filter(usuario=usuario)
---
+++ Modulo 7/Individual 2/toDoList/app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('login/', views.login_view, name='login'),
+    path('home/', views.home_view, name='home'),
+
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+
+]
